78b2a7799221635e4ba2cade772c1fddf3ceccf0
RealPodonok <real1podonok@gmail.com>
2025-05-25 23:21:00 +0500

    Delete collision.js

--- collision.js
+++
@@ -1,8 +0,0 @@
-// collision.js
-
-export function isColliding(a, b, range = 10) {
-  const dx = a.x - b.x;
-  const dy = a.y - b.y;
-  const distance = Math.sqrt(dx * dx + dy * dy);
-  return distance < range;
-}
